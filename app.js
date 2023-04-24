1d956cce6d170724831fa5ee0599949aef3e35b6
Inna Iazovskaia <inna.0961@mail.ru>
2023-04-24 17:25:20 +0200

    Implement inquirerMenu and crete cases for option 1 and 2

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 require("colors");
-const { showMenu, pause } = require("./helpers/messages");
-
-console.clear();
+const {
+  inquirerMenu,
+  inquirerPause,
+  reedInput,
+} = require("./helpers/inquirer");
+const Tasks = require("./models/tasks");
 
 const main = async () => {
-  showMenu();
-  pause();
+  let option = "";
+  const tasks = new Tasks();
+
+  do {
+    option = await inquirerMenu();
+
+    switch (option) {
+      case "1":
+        const description = await reedInput("Description:");
+        tasks.createTask(description);
+        break;
+
+      case "2":
+        console.log(tasks.listArr());
+        break;
+    }
+
+    await inquirerPause();
+  } while (option !== "0");
 };
 
 main();
